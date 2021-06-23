05d9efa0a9c771b8622dcf0d62053e48e8943485
Anastasya <aamokeeva@yandex.ru>
2021-06-23 20:26:07 +0300

    Настраивает сбор пользовательских данных с форм и отправку на локальный сервер

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-const dist = "./dist/";
+// const dist = "./dist/";
+const dist  = "C:/MAMP/htdocs/test";
 
 const ghPages = require('gh-pages');
 const path = require('path');
