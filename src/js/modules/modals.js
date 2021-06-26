9a47c0568b7e1c36a4e0c81f1371e84c01ee0615
Anastasya <aamokeeva@yandex.ru>
2021-06-26 22:07:12 +0300

    Вносит небольшие улучшения

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,7 +3,9 @@ const modals = () => {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
             close = document.querySelector(closeSelector),
-            windows = document.querySelectorAll('[data-modal]');
+            windows = document.querySelectorAll('[data-modal]'),
+            // исправляет прыгающий скролл при вызове модального окна
+            scroll = calcScroll();
 
         trigger.forEach(item => {
             item.addEventListener('click', (evt) => {
@@ -18,6 +20,8 @@ const modals = () => {
                 modal.style.display = "block";
                 document.body.style.overflow = 'hidden';
                 // document.body.classList.add('modal-open');
+
+                document.body.style.marginRight = `${scroll}px`;
             });
         });
 
@@ -29,6 +33,8 @@ const modals = () => {
             modal.style.display = "none";
             document.body.style.overflow = '';
             // document.body.classList.remove('modal-open');
+
+            document.body.style.marginRight = `0px`;
         });
 
         modal.addEventListener('click', (evt) => {
@@ -40,6 +46,8 @@ const modals = () => {
                 modal.style.display = "none";
                 document.body.style.overflow = '';
                 // document.body.classList.remove('modal-open');
+
+                document.body.style.marginRight = `0px`;
             }
         });
     }
@@ -49,6 +57,23 @@ const modals = () => {
             document.querySelector(selector).style.display = 'block';
             document.body.style.overflow = 'hidden';
         }, time);
+    }
+
+    // вспомогательная функция - рассчитывает ширину области прыгающего при вызове модального окна скролла
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
     }
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
